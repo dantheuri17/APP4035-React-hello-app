51397c71aa2c970d47c47fcb2e77ca68a37aa62b
Daniel Theuri <dantheuri17@gmail.com>
2023-03-22 09:25:54 +0300

    adding submitted state to contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,35 +1,55 @@
-import React from 'react'
-import {useState} from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Contact = () => {
+	const [data, setData] = useState({});
+	const [submitted, setSubmitted] = useState(false);
 
-    const [data, setData] = useState('');
+	const serverHost = "http://localhost:4000";
 
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        //create an object for the current input field event 
-        const currentInputFieldData = {
-            [name]: value
-        }
-        const updatedData = {
-            ...data, 
-            ...currentInputFieldData
-        }
-        setData(updatedData);
-    }
-  const handleSubmit = (e) => {
-    console.log(data);
-    e.preventDefault();
-  }
+	async function addStudent(student) {
+		const url = serverHost + "/students";
+		const options = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(student),
+		};
+		const response = await fetch(url, options);
+		if (response.status === 200) {
+			setSubmitted(true);
+		}
+	}
 
-  return (
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		//create an object for the current input field event
+		const currentInputFieldData = {
+			[name]: value,
+		};
+		const updatedData = {
+			...data,
+			...currentInputFieldData,
+		};
+		setData(updatedData);
+	};
+	const handleSubmit = (e) => {
+		console.log(data);
+		addStudent(data);
+		e.preventDefault();
+		
+	};
+
+	return (
 		<div>
 			<h1>This is the Contact Page</h1>
+			{!submitted ?
 			<form onSubmit={handleSubmit}>
 				<label>
 					First name:
-					<input name="firstName" type="text" onChange= {handleChange}/>
+					<input name="firstName" type="text" onChange={handleChange} />
 				</label>
 				<br />
 				<label>
@@ -44,8 +64,11 @@ const Contact = () => {
 				<br />
 				<input type="submit" value="submit" />
 			</form>
+			:
+			<p>Your data has been submitted successfully. Thank you</p> 
+	}
 		</div>
 	);
-}
+};
 
-export default Contact
+export default Contact;
